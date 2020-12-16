a33b1e739bfb35383da86f2cbb02bd95ca678afd
Gossteer <gossteerplus@gmail.com>
2020-12-16 15:49:41 +0300

    pamagite

--- src/core/scatter_hist.py
+++ src/core/scatter_hist.py
@@ -7,6 +7,8 @@ import difflib
 import PIL
 import matplotlib.pyplot as plt
 from wand.image import Image
+from wand.color import Color
+from wand.drawing import Drawing
 from wand.display import display
 import pdf2image as pdf
 from tkinter import filedialog
@@ -66,21 +68,22 @@ def OCRRender(FirstFile, SecondFile):
     # cv.destroyAllWindows()
 
 def get_path():
-    root = Tk()
-    root.withdraw()
-    path = filedialog.askopenfiles(initialdir = "/Documents",title = "Выберете 2 pdf/image файла",filetypes = (("Файлы форматов:","*.jpg *.png *.pdf"),("pdf files","*.pdf")))
-    # Нужна проверка на количество пришедших файлов
-    root.destroy()
-    for pathName in path:
-        extension = pathName.name.split('.')[-1]
-        if extension == 'pdf':
-            files.append(ExtractPDF(pathName.name))
-            # Найти решение для того, чтобы не сохраняя, сразу переводить фай
-            # Если всё же сохранять, то не забывать, что может быть два pdf файла 
-        else:
-            files.append(pathName.name)
+    # root = Tk()
+    # root.withdraw()
+    # path = filedialog.askopenfiles(initialdir = "/Documents",title = "Выберете 2 pdf/image файла",filetypes = (("Файлы форматов:","*.jpg *.png *.pdf"),("pdf files","*.pdf")))
+    # # Нужна проверка на количество пришедших файлов
+    # root.destroy()
+    # for pathName in path:
+    #     extension = pathName.name.split('.')[-1]
+    #     if extension == 'pdf':
+    #         files.append(ExtractPDF(pathName.name))
+    #         # Найти решение для того, чтобы не сохраняя, сразу переводить фай
+    #         # Если всё же сохранять, то не забывать, что может быть два pdf файла 
+    #     else:
+    #         files.append(pathName.name)
    
-    show(files[0], files[1])
+    # show(files[0], files[1])
+    show("./image_buf/551.pdf.png", "./image_buf/493.pdf.png")
     # os.remove('./image_buf/buf.png')
 
 def show(FirstFile, SecondFile):
@@ -89,27 +92,66 @@ def show(FirstFile, SecondFile):
     ax_histy = fig.add_axes(rect_histy)
     ax_histy.tick_params(axis="x", labelbottom=False)
 
-    # OCRRender(FirstFile, SecondFile)
-    wondRender(FirstFile, SecondFile)
+    OCRRender(FirstFile, SecondFile)
+    # wondRender(FirstFile, SecondFile)
     addcomare(ax, result_image_gloval)
-    addlegeng(ax)
-    addbar(ax_histy, result_metric_global)
+    # addlegeng(ax)
+    # addbar(ax_histy, result_metric_global)
     plt.show()
 
 def wondRender(FirstFile, SecondFile):
     global result_image_gloval
     global result_metric_global
     with Image(filename=FirstFile) as base:
-            with Image(filename=SecondFile) as img:
-                base.fuzz = base.quantum_range * 0.20  # Threshold of 20%
-                result_image, result_metric = base.compare(img, metric='normalized_cross_correlation')
-                # result_image.save(filename='./images/Test/buf.png')
+        with Image(filename=SecondFile) as img:
+            # base.fuzz = base.quantum_range * 0.2  # Threshold of 20%
+            # result_image, result_metric = base.compare(img, metric='normalized_cross_correlation', highlight = Color('green'))
+            result_image, result_metric = base.compare(img, metric='normalized_cross_correlation')
+            result_image.save(filename='./images/Test/' + FirstFile.split('/')[-1]+SecondFile.split('/')[-1])
     result_image_gloval = result_image
-    result_metric_global = result_metric
+    result_metric_global = result_metric    
+    # with Image(filename=FirstFile) as imgg:
+    #     imgg.fuzz = imgg.quantum_range * 0.1
+    #     imgg.background_color = 'black'
+    #     mbr = imgg.minimum_bounding_box()
+    #     with Drawing() as ctx:
+    #         ctx.fill_color = 'transparent'
+    #         ctx.stroke_color = 'red'
+    #         ctx.polygon(points=mbr['points'])
+    #         ctx.fill_color = 'red'
+    #         ctx.stroke_color = 'transparent'
+    #         ctx.text(1, 10, '{0:.4g}°'.format(mbr['angle']))
+    #         ctx(imgg)
+    #     imgg.save(filename='./images/Test/kdf_black_mbr.png')
+    # dissimilarity_threshold = 0.318
+    # similarity_threshold = 1.0
+    # with Image(filename=FirstFile) as imgg:
+    #     with Image(filename=SecondFile) as reference:
+    #         location, diff = imgg.similarity(reference,
+    #                                         similarity_threshold, metric='normalized_cross_correlation')
+    #         if diff > dissimilarity_threshold:
+    #             print('Images too dissimilar to match')
+    #         elif diff <= similarity_threshold:
+    #             print('First match @ {left}x{top}'.format(**location))
+    #         else:
+    #             print('Best match @ {left}x{top}'.format(**location))
+    # with Image(filename=FirstFile) as imgg:
+    #     with Image(filename=SecondFile) as phase:
+    #         imgg.inverse_fourier_transform(phase)
+    #     imgg.save(filename='./images/Test/kdff_black_mbr.png')
+    # with Image(filename=FirstFile) as imgg:
+    #     objects = imgg.connected_components()
+    # for cc_obj in objects:
+    #     print("{0._id}: {0.size} {0.offset}".format(cc_obj))
+    with Image(filename=FirstFile) as imgA:
+        with Image(filename=SecondFile) as imgB:
+            imgA.sequence.append(imgB)
+        with imgA.complex('conjugate') as results:
+            results.save(filename='./images/Test/kdfff_black_mbr.png')
 
 def addcomare(ax, result_image):
     ax.imshow(result_image)
-    ax.set_title('/Название файлов сравнения/')
+    # ax.set_title('/Название файлов сравнения/')
 
 def addlegeng(ax):
     ax.plot(1, color='gray' , label='Одинаково')
@@ -124,9 +166,11 @@ def addbar(ax_histy, result_metric):
     ax_histy.bar(1,data_2, color='red')
 
 def ExtractPDF(File):
+    # print(File.split('/')[-1])
     images = pdf.convert_from_path(File,poppler_path = r"./poppler-0.68.0/bin")
-    images[0].save("./image_buf/buf.png","png")
-    return "./image_buf/buf.png"
+    pathNewFilePng = "./image_buf/"+ File.split('/')[-1] +".png"
+    images[0].save(pathNewFilePng,"png")
+    return pathNewFilePng
 
 
 get_path()
